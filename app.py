5678ab91ed6f466324aa7b3494eb4b1f03c1fb96
Oséas de Jesus <oseasjs@gmail.com>
2022-04-05 20:27:23 -0300

    Adding simple test to validate CI config

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from src.controller import task_controller
 
 from src import *
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, port=5001)
---
+++ src/service/task_service_test.py
@@ -0,0 +1,7 @@
+def test_sum():
+  """ Simple test """
+  assert sum([1, 2, 3]) == 6
+
+if __name__ == "__main__":
+    test_sum()
+    print("Everything passed")
