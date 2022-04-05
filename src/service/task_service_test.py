b031b07fc1d41ecd4d330f97d62649a5660b8c77
Oséas de Jesus <oseasjs@gmail.com>
2022-04-05 20:35:02 -0300

    Removed unused code on test

--- src/service/task_service_test.py
+++ src/service/task_service_test.py
@@ -1,7 +1,3 @@
 def test_sum():
-  """ Simple test """
-  assert sum([1, 2, 3]) == 6
-
-if __name__ == "__main__":
-    test_sum()
-    print("Everything passed")
+    """Simple test"""
+    assert sum([1, 2, 3]) == 6
